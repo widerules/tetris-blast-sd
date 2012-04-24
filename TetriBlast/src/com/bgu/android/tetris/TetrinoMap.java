25b521224f44652c4c145cc9d3a020a665f35cde
surkesden@gmail.com <surkesden@gmail.com@1a717bcb-a307-3649-c74a-2359bfb9b4ad>
2012-04-24 19:44:59 +0000

    added some basic flour check

--- TetriBlast/src/com/bgu/android/tetris/TetrinoMap.java
+++ TetriBlast/src/com/bgu/android/tetris/TetrinoMap.java
@@ -24,16 +24,44 @@ public class TetrinoMap {
 		}
 	}
 	
-	public void putTetrinoOnMap(Tetrino shape) {
+	/**
+	 * This function puts the tetrino on the map 
+	 * and check collision with the other objects on a map
+	 * @param shape - tetrino to put
+	 * @return true if puts successful otherwise false
+	 */
+	public boolean putTetrinoOnMap(Tetrino shape) {
 		for(int col = 0; col < shape.getSize(); col++){
 			for(int row = 0; row < shape.getSize(); row++) {
-				//TODO need to check map bounds
-				map[shape.getXPos()+col][shape.getYPos()+row] = shape.sMap[col][row];
+			//TODO need to check map bounds collision detect
+				if (shape.sMap[col][row] != TileView.BLOCK_EMPTY) {
+					if (shape.getXPos() + col >= TetrinoMap.MAP_X_SIZE || 
+							shape.getYPos() + row >= TetrinoMap.MAP_Y_SIZE ||
+							map[shape.getXPos()+col][shape.getYPos()+row] != TileView.BLOCK_EMPTY)
+						return false;
+				}
 			}
 		}
+		for(int col = 0; col < shape.getSize(); col++){
+			for(int row = 0; row < shape.getSize(); row++) {
+				if (shape.sMap[col][row] != TileView.BLOCK_EMPTY) 
+					map[shape.getXPos()+col][shape.getYPos()+row] = shape.sMap[col][row];
+				}	
+			}
+		return true;
 	}
+	
 	
 	public int getMapValue(int x, int y) {
 		return map[x][y];
+	}
+	
+	public void copyFrom(TetrinoMap srcMap) {
+		for (int x = 0; x < TetrinoMap.MAP_X_SIZE; x++) {
+			for(int y = 0; y < TetrinoMap.MAP_Y_SIZE; y++) {
+				this.map[x][y] = srcMap.getMapValue(x, y);
+			}
+		}
+		
 	}
 }
