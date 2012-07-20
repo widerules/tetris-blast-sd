137f7e41809bc66ea4c48548f6959fd7cd8a6704
surkesden <surkesden@gmail.com>
2012-07-20 06:42:05 +0000

    end game logic added

--- TetriBlast/src/com/bgu/android/tetris/BluetoothConnectivity.java
+++ TetriBlast/src/com/bgu/android/tetris/BluetoothConnectivity.java
@@ -44,7 +44,7 @@ public class BluetoothConnectivity {
     public static final int TYPE_SCORE = 2;
     public static final int TYPE_START = 3;
     public static final int TYPE_GAME_STATE = 4;
-    public static final int TYPE_WIN_LOSS = 5;
+    public static final int TYPE_LOSS = 5;
     public static final int TYPE_NAME = 6;
     public static final int TYPE_DIFFICULTY = 7;//BT message type that set the difficulty of the game
     public static final int TYPE_SHADOW = 8;//BT message type that set shadow on/off
